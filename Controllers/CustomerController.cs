71c30917f65bc1c5da3ad41de2c51b8683765b99
bsef11a021 <bsef11a021@pucit.edu.pk>
2014-06-27 01:19:30 +0500

    contoller

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,197 @@
+﻿using Baichday.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Linq.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Baichday.Controllers
+{
+    public class CustomerController : Controller
+    {
+        ICustomer1 cu;
+        //
+        // GET: /Add/
+        public CustomerController(ICustomer1 c)
+        {
+            cu = c;
+        }
+
+
+        public ActionResult sign()
+        {
+            return View();
+        }
+        public ActionResult Success()
+        {
+            return View();
+        }
+        public ActionResult sign1()
+        {
+            return View();
+        }
+        public ActionResult ManagePost()
+        {
+            
+            
+            int cid=getID();
+            var q = cu.ManagePost(cid);
+            
+                return View(q);
+           
+        }
+        public ActionResult delete(int id)
+        {
+            cu.delete(id);
+            return RedirectToAction("ManagePost");
+            
+        }
+        public ActionResult edit(int id)
+        {
+           Add q= cu.edit(id);
+            
+            return View(q);
+
+        }
+        public ActionResult sign2()
+        {
+            return View();
+        }
+        public JsonResult checkUserName()
+        {
+            string name = Request["u"];
+            if(cu.checkUserName(name))
+            {
+                return this.Json(true, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+
+
+
+                return this.Json(false, JsonRequestBehavior.AllowGet);
+            }
+        }
+        public ActionResult signin(Customer c)
+        {
+            string nm = c.Name;
+           
+            
+
+            Session.Add("name", nm);
+
+
+            if (ModelState.IsValid)
+            {
+                if (cu.signin(c))
+                {
+                    return View("AddPost");
+                }
+                else
+                {
+
+                    return View("Failure");
+
+                }
+            }
+            else
+            {
+                return View("Failure");
+            }
+
+
+        }
+        public ActionResult signin1(Customer c)
+        {
+            string nm = c.Name;
+          
+
+
+            Session.Add("name", nm);
+
+           
+
+            if (cu.signin1(c))
+            {
+                return RedirectToAction("ManagePost");
+            }
+            else
+            {
+
+                return View("Failure");
+
+            }
+
+
+        }
+        public ActionResult signup(Customer c)
+        {
+            cu.signup(c);
+
+
+            return View("sign1");
+        }
+        public ActionResult signup1(Customer c)
+        {
+            cu.signin1(c);
+
+
+            
+
+
+            return View("sign2");
+        }
+        public ActionResult AddPost1(Add a)
+        {
+
+           
+                a.Cid = getID();
+                string p = Request["picture"];
+
+
+                HttpPostedFileBase file = Request.Files[0];
+                var path = Path.Combine(Server.MapPath("~/images/"), file.FileName);
+                file.SaveAs(path);
+
+                a.image = file.FileName;
+
+                if (ModelState.IsValid)
+                {
+                    cu.AddPost1(a);
+                }
+                else
+                {
+                    return View("AddPost");
+                }
+           
+            return View("Success");
+        }
+        private int getID()
+        {
+
+            Database1Entities3 c = new Database1Entities3();
+            string name=Session["name"].ToString();
+            
+            var q = c.Customers.First(x => x.Name.Equals(name));
+            return (q.Id);
+        }
+        public ActionResult editPost(Add a)
+        {
+
+            HttpPostedFileBase file = Request.Files[0];
+            var path = Path.Combine(Server.MapPath("~/images/"), file.FileName);
+            file.SaveAs(path);
+
+            a.image = file.FileName;
+            cu.editPost(a);
+            return RedirectToAction("Success");
+        
+        }
+        public ActionResult Failure()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,205 @@
+﻿using Baichday.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Linq.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Baichday.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        IHome1 i;
+        public HomeController(IHome1 iu)
+        {
+            i = iu;
+        }
+
+        public ActionResult index()
+        {
+
+            var q = i.index();
+            return View(q);
+        }
+        public ActionResult subpage()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Help()
+        {
+            return View();
+        }
+        public ActionResult Success()
+        {
+            return View();
+        }
+        public ActionResult postShow(int id)
+        {
+           Add q= i.postShow(id);
+           Session.Add("id", id);
+           List<Comment> co = i.showComment(id);
+           ViewBag.l = co;
+
+            return View(q);
+        }
+        public JsonResult search12()
+        {
+            string city = Request["u"];
+
+            List<Add> q = i.search12("lahore");
+           
+            
+           // List<Add> w= i.search12(city);
+
+            return this.Json(q, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult search13()
+        {
+            string city = Request["u"];
+
+            List<Add> q = i.search12("islamabad");
+
+
+            // List<Add> w= i.search12(city);
+
+            return this.Json(q, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult search14()
+        {
+            string city = Request["u"];
+
+            List<Add> q = i.search12("karachi");
+
+
+            // List<Add> w= i.search12(city);
+
+            return this.Json(q, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult price1()
+        {
+            string city = Request["u"];
+
+            List<Add> q = i.price1();
+
+
+            // List<Add> w= i.search12(city);
+
+            return this.Json(q, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult price2()
+        {
+            string city = Request["u"];
+
+            List<Add> q = i.price2();
+
+
+            // List<Add> w= i.search12(city);
+
+            return this.Json(q, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult price3()
+        {
+            string city = Request["u"];
+
+            List<Add> q = i.price3();
+
+
+            // List<Add> w= i.search12(city);
+
+            return this.Json(q, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult cat1()
+        {
+            string city = Request["u"];
+
+            List<Add> q = i.cat1();
+
+
+            // List<Add> w= i.search12(city);
+
+            return this.Json(q, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult cat4()
+        {
+            string city = Request["u"];
+
+            List<Add> q = i.cat4();
+
+
+            // List<Add> w= i.search12(city);
+
+            return this.Json(q, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult cat2()
+        {
+            string city = Request["u"];
+
+            List<Add> q = i.cat2();
+
+
+            // List<Add> w= i.search12(city);
+
+            return this.Json(q, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult cat3()
+        {
+            string city = Request["u"];
+
+            List<Add> q = i.cat3();
+
+
+            // List<Add> w= i.search12(city);
+
+            return this.Json(q, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult search()
+        {
+            string city = Request["u"];
+           var q= i.search(city);
+           return View(q);
+
+        }
+      
+       
+        public ActionResult category()
+        {
+
+            string cat = Request["u"];
+            var q = i.category(cat);
+            
+                return View(q);
+            
+            
+        }
+        public ActionResult Search1()
+        {
+            return View();
+        }
+        public ActionResult price()
+        {
+            string p = Request["u"];
+            var q=i.price(p);
+            
+                return View(q);
+            
+
+
+        }
+        public ActionResult addComment(Comment c)
+        {
+            c.Aid = (int)Session["id"];
+            i.addComment(c);
+            return RedirectToAction("Success");
+        }
+       
+
+    }
+}
